06829d0239194d1c2873944be91fba304c5e65d3
Катенцев Андрей <hyperlink@inbox.ru>
2018-07-01 15:40:31 +0300

    'UserSessionKeyFileCipher' module added.

---
+++ Kernel/UserSessionKeyFileCipher.cpp
@@ -0,0 +1,47 @@
+#include "UserSessionKeyFileCipher.h"
+
+#include "KAA/include/exception/operation_failure.h"
+
+#include "FileProgressHandler.h"
+
+namespace KAA
+{
+	namespace FileSecurity
+	{
+		UserSessionKeyFileCipher::UserSessionKeyFileCipher(const std::shared_ptr<filesystem::driver> filesystem) :
+		filesystem(filesystem),
+		cipher_progress(nullptr)
+		{
+			if (nullptr == filesystem)
+			{
+				const std::wstring source { __FUNCTIONW__ };
+				const std::wstring description { L"failed to create 'user session key' file cipher class instance" };
+				constexpr auto reason = operation_failure::R_INVALID_ARGUMENT;
+				constexpr auto severity = operation_failure::S_ERROR;
+				throw operation_failure(source, description, reason, severity);
+			}
+		}
+
+		void UserSessionKeyFileCipher::IEncryptFile(const std::wstring& file_to_encrypt_path, const std::wstring& key_file_path)
+		{
+			throw operation_failure(__FUNCTIONW__, L"cannot encrypt file", operation_failure::R_NOT_IMPLEMENTED, operation_failure::S_ERROR);
+		}
+
+		void UserSessionKeyFileCipher::IDecryptFile(const std::wstring& file_to_decrypt_path, const std::wstring& key_file_path)
+		{
+			throw operation_failure(__FUNCTIONW__, L"cannot decrypt file", operation_failure::R_NOT_IMPLEMENTED, operation_failure::S_ERROR);
+		}
+
+		FileProgressHandler* UserSessionKeyFileCipher::ISetProgressCallback(FileProgressHandler* handler)
+		{
+			const auto previous = cipher_progress;
+			cipher_progress = handler;
+			return previous;
+		}
+
+		progress_state UserSessionKeyFileCipher::ChunkProcessed(const uint64_t total_bytes_processed, const uint64_t total_file_size)
+		{
+			return cipher_progress ? cipher_progress->ChunkProcessed(total_bytes_processed, total_file_size) : progress_quiet;
+		}
+	}
+}
---
+++ Kernel/UserSessionKeyFileCipher.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <memory>
+
+#include "KAA/include/progress_state.h"
+
+#include "FileCipher.h"
+
+namespace KAA
+{
+	namespace filesystem
+	{
+		class driver;
+	}
+
+	namespace FileSecurity
+	{
+		class FileProgressHandler;
+
+		// FUTURE: KAA: std::shared_ptr.
+		// TODO: KAA: remove commented code.
+		class UserSessionKeyFileCipher final : public FileCipher
+		{
+		public:
+			explicit UserSessionKeyFileCipher(std::shared_ptr<filesystem::driver>);
+
+			/*UserSessionKeyFileCipher(const UserSessionKeyFileCipher&) = delete;
+			UserSessionKeyFileCipher(UserSessionKeyFileCipher&&) = delete;
+			UserSessionKeyFileCipher& operator = (const UserSessionKeyFileCipher&) = delete;
+			UserSessionKeyFileCipher& operator = (UserSessionKeyFileCipher&&) = delete;*/
+
+		private:
+			std::shared_ptr<filesystem::driver> filesystem;
+			FileProgressHandler* cipher_progress;
+
+			void IEncryptFile(const std::wstring&, const std::wstring&) override;
+			void IDecryptFile(const std::wstring&, const std::wstring&) override;
+
+			FileProgressHandler* ISetProgressCallback(FileProgressHandler*) override;
+
+			progress_state ChunkProcessed(uint64_t total_bytes_processed, uint64_t total_file_size);
+		};
+	}
+}
